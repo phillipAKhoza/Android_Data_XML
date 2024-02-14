072bc356616cf0417010103bc16fbf98f672b747
PhillipAKhoza <phillipantonk@gmail.com>
2024-02-14 19:30:31 +0200

    calculation

--- app/src/main/java/com/phillipdev/senddatabtwnactivitynfragment/BrickResultFragment.kt
+++ app/src/main/java/com/phillipdev/senddatabtwnactivitynfragment/BrickResultFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class BrickResultFragment : Fragment() {
-
+    lateinit var tvReults : TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view : View = inflater.inflate(R.layout.fragment_brick_result, container, false)
+        tvReults = view.findViewById(R.id.textViewResult)
 
+        val length : Double = arguments?.getDouble("length").toString().toDouble()
+        val width: Double = arguments?.getDouble("width").toString().toDouble()
+        val result : Double = (length*width) * 60
+        tvReults.text = "You need $result Bricks"
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_brick_result, container, false)
+        return view
     }
 
 
