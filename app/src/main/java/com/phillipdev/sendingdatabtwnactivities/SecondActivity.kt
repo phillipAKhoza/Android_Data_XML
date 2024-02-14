425051322ae4753608c68ad9170e9113205d1c9e
phillipAKhoza <phillipantonk@gmail.com>
2024-02-14 14:32:16 +0200

    display data

--- app/src/main/java/com/phillipdev/sendingdatabtwnactivities/SecondActivity.kt
+++ app/src/main/java/com/phillipdev/sendingdatabtwnactivities/SecondActivity.kt
@@ -2,10 +2,28 @@ package com.phillipdev.sendingdatabtwnactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var tvName : TextView
+    lateinit var tvEmail: TextView
+    lateinit var tvPhone : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        tvName = findViewById(R.id.textViewName)
+        tvEmail = findViewById(R.id.textViewEmail)
+        tvPhone = findViewById(R.id.textViewPhone)
+
+        val name = intent.getStringExtra("name")
+        val email = intent.getStringExtra("email")
+        val phone = intent.getLongExtra("phone",0)
+
+        tvName.text = "Hello $name"
+        tvEmail.text = "Your email is $email"
+        tvPhone.text= "Your Phone number is $phone"
+
     }
 }
