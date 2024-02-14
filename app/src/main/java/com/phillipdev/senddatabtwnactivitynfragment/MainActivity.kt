c33c31d14d01c816fe6c4c9e18cdc15efb512b4a
PhillipAKhoza <phillipantonk@gmail.com>
2024-02-14 19:10:28 +0200

    add variables and initialise them

--- app/src/main/java/com/phillipdev/senddatabtwnactivitynfragment/MainActivity.kt
+++ app/src/main/java/com/phillipdev/senddatabtwnactivitynfragment/MainActivity.kt
@@ -2,10 +2,24 @@ package com.phillipdev.senddatabtwnactivitynfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etLenght : EditText
+    lateinit var etWidth : EditText
+    lateinit var btnCalculate : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etLenght = findViewById(R.id.editTextLength)
+        etWidth = findViewById(R.id.editTextWidth)
+        btnCalculate = findViewById(R.id.btnCalc)
+
+        btnCalculate.setOnClickListener {
+
+        }
+
     }
 }
